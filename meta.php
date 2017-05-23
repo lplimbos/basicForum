ece81ad570ac977991cd300269dc1921137ac8ee
lplimbos <lplimbos@gmail.com>
2017-05-23 12:05:59 -0500

    added the jquery library locally

--- meta.php
+++ meta.php
@@ -3,5 +3,5 @@
 <link rel="stylesheet" type="text/css" href="styles/norm.css">
 <link rel="stylesheet" type="text/css" href="styles/style.css">
 <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="scripts/jquery-3.2.1.min.js"></script>
 <script type="text/javascript" src="scripts/script.js"></script>
