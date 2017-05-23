062a86a4bb405a78a4b6dd659545b1b140ab4741
lplimbos <lplimbos@gmail.com>
2017-05-23 11:40:35 -0500

    More changes to presentation

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
 
 
-	var img = getRandomIntInclusive(1,5);
+	var img = getRandomIntInclusive(1,9);
 
 	$(document.body).css({'background':'url(images/background-' + img + '.jpg) no-repeat center center fixed','-webkit-background-size': 'cover',
     '-moz-background-size' : 'cover',
     '-o-background-size' : 'cover',
     'background-size' : 'cover'});
 
-	$('.selectedItem').parent().css({'border-bottom-style' : 'solid', 'border-bottom-width' : '1px', 'border-bottom-color' : 'red'});
+	$('.selectedItem').parent().css({'border-bottom-style' : 'solid', 'border-bottom-width' : '2px', 'border-bottom-color' : 'red'});
 });
 
 function getRandomIntInclusive(min, max) {
